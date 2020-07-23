df51657b88303b811408b52087c904560f8d68e9
Richie Casto <rcasto92@gmail.com>
2020-07-23 14:28:03 -0400

    stashing progress

--- src/infinite-scroll.js
+++ src/infinite-scroll.js
@@ -1,14 +1,25 @@
 export default class InfiniteScroll extends HTMLElement {
-    static thresholdLimit = .85;
     static template = `
+        <style>
+            div {
+                overflow-y: auto;
+            }
+        </style>
         <div>
-            <slot>No content to scroll</slot>
+            <slot></slot>
         </div>
     `;
     /**
      * @type {HTMLTemplateElement}
      */
     static templateElem = null;
+
+    static get observedAttributes() {
+        return [
+          'data-height',
+          'data-threshold'
+        ];
+      }
 
     constructor() {
         super();
@@ -17,6 +28,10 @@ export default class InfiniteScroll extends HTMLElement {
             InfiniteScroll.templateElem = document.createElement('template');
             InfiniteScroll.templateElem.innerHTML = InfiniteScroll.template;
         }
+
+        this.divContentElem = null;
+        this.thresholdLimit = 0.85;
+        this.divContainerHeight = null;
     }
     connectedCallback() {
         const shadowRoot = this.attachShadow({
@@ -26,18 +41,21 @@ export default class InfiniteScroll extends HTMLElement {
 
         shadowRoot.appendChild(templateClone);
 
+        this.divContentElem = shadowRoot.querySelector('div');
+        this.setDivContainerHeight();
+
         window.addEventListener('scroll', () => {
             let ticking = false;
 
             if (!ticking) {
                 window.requestAnimationFrame(() => {
-                    const currentThreshold = (document.body.scrollTop + document.body.clientHeight) / document.body.scrollHeight;
+                    const currentThreshold = (window.scrollY + window.innerHeight) / this.divContentElem.scrollHeight;
 
                     // May only want to fire once, when the threshold is reached
                     // As of now it would fire mutliple times
-                    if (currentThreshold >= InfiniteScroll.thresholdLimit) {
+                    if (currentThreshold >= this.thresholdLimit) {
                         const event = new Event('infinite-scroll-fetch');
-                        document.body.dispatchEvent(event);
+                        window.dispatchEvent(event);
                     }
 
                     ticking = false;
@@ -46,6 +64,23 @@ export default class InfiniteScroll extends HTMLElement {
                 ticking = true;
             }
         });
+    }
+    attributeChangedCallback(name, _, newValue) {
+        if (name === 'data-height') {
+            this.setDivContainerHeight(newValue);
+        } else if (name === 'data-threshold') {
+            const thresholdLimit = parseFloat(newValue) || this.thresholdLimit;
+            if (thresholdLimit >= 0 && thresholdLimit <= 1) {
+                this.thresholdLimit = thresholdLimit;
+            }
+        }
+    }
+    setDivContainerHeight(height = this.divContainerHeight) {
+        this.divContainerHeight = height;
+        if (!this.divContentElem) {
+            return;
+        }
+        this.divContentElem.style.height = height;
     }
 }
 
