3b1a0efc31780c67b124d46bbe7d989682bf2a99
Richie Casto <rcasto92@gmail.com>
2020-07-24 09:21:11 -0400

    add some more tests around connectedCallback

--- tests/infinite-scroll.test.js
+++ tests/infinite-scroll.test.js
@@ -11,14 +11,34 @@ describe('infinite-scroll Tests', () => {
     });
 
     it('can initialize - init static template only once', () => {
-        const mockCreateElementSpy = jest.spyOn(document, 'createElement');
+        const createElementSpy = jest.spyOn(document, 'createElement');
 
         InfinteScroll.templateElem = null;
 
         new InfinteScroll();
         new InfinteScroll();
 
-        expect(mockCreateElementSpy).toHaveBeenCalledTimes(1);
+        expect(createElementSpy).toHaveBeenCalledTimes(1);
         expect(InfinteScroll.templateElem.innerHTML).toEqual(InfinteScroll.template);
+    });
+
+    it('connectedCallback - sets up shadow DOM', () => {
+        const infiniteScroll = new InfinteScroll();
+
+        expect(infiniteScroll.shadowRoot).toBeNull();
+
+        infiniteScroll.connectedCallback();
+
+        expect(infiniteScroll.shadowRoot).toBeDefined();
+        expect(infiniteScroll.divContentElem).toBeDefined();
+    });
+
+    it('connectedCallback - call internal setDivContainerHeight', () => {
+        const infiniteScroll = new InfinteScroll();
+        const setDivContainerHeightSpy = jest.spyOn(infiniteScroll, 'setDivContainerHeight');
+
+        infiniteScroll.connectedCallback();
+
+        expect(setDivContainerHeightSpy).toHaveBeenCalledTimes(1);
     });
 });
